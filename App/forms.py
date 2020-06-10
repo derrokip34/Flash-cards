acd39bcc181b50ed1a0333f3c74ec2c444d55652
derrickip34 <derrickip34@gmail.com>
2020-06-10 15:00:20 +0300

    allow a user to post a flashcard

---
+++ App/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import FlashCard
+
+class PostFlashCard(forms.ModelForm):
+    class Meta:
+        model = FlashCard
+        exclude = ['user']
+        widgets = {
+            'card_notes':forms.Textarea()
+        }
--- App/models.py
+++ App/models.py
@@ -6,5 +6,6 @@ class FlashCard(models.Model):
     card_title = models.CharField(max_length=50)
     card_notes = models.CharField(max_length=250)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    subject = models.CharField(max_length=30)
     posted_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
